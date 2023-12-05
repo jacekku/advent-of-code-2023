4e3adbdbccd261d6e1e22cd1d1884096158c9b5f
jacekku <jacekwyroba97@gmail.com>
2023-12-05 18:51:03 +0100

    Day 5 part 2 naive

---
+++ day5/js/solution2.js
@@ -0,0 +1,127 @@
+console.time('s')
+const fs = require('node:fs');
+const { parse } = require('node:path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('../input act'), crlfDelay: Infinity
+});
+rl.on('line', (line) => { eachLine(line) });
+rl.on('close', () => {
+    onClose()
+    console.timeEnd('s')
+})
+// #####################################
+
+class MapParserInator {
+    seeds = []
+
+    /**
+     * @typedef AlmanacMap 
+     * @property {number} dst
+     * @property {number} src
+     * @property {number} len
+     */
+    /**  @type AlmanacMap[] */
+    seedToSoil = []
+    soilToFertilizer = []
+    fertilizerToWater = []
+    waterToLight = []
+    lightToTemperature = []
+    temperatureToHumidity = []
+    humidityToLocation = []
+
+    currentParser = this.seedParser
+    emptyLine(line) { return line == "" }
+    /** @param {string} line */
+    changeParser(line) {
+        if (line.startsWith("seed-to-soil")) {
+            return this.currentParser = (line) => this.mapParser(line, this.seedToSoil)
+        }
+        if (line.startsWith("soil-to-fertilizer")) {
+            return this.currentParser = (line) => this.mapParser(line, this.soilToFertilizer)
+        }
+        if (line.startsWith("fertilizer-to-water")) {
+            return this.currentParser = line => this.mapParser(line, this.fertilizerToWater)
+        }
+        if (line.startsWith("water-to-light")) {
+            return this.currentParser = line => this.mapParser(line, this.waterToLight)
+        }
+        if (line.startsWith("light-to-temperature")) {
+            return this.currentParser = line => this.mapParser(line, this.lightToTemperature)
+        }
+        if (line.startsWith("temperature-to-humidity")) {
+            return this.currentParser = line => this.mapParser(line, this.temperatureToHumidity)
+        }
+        if (line.startsWith("humidity-to-location")) {
+            return this.currentParser = line => this.mapParser(line, this.humidityToLocation)
+        }
+        return false
+    }
+    /** @param {string} line */
+    parse(line) {
+        if (this.emptyLine(line)) return
+        if (this.changeParser(line)) return
+        this.currentParser(line)
+    }
+
+    /** @param {string} line */
+    seedParser(line) {
+        const seeds = line.split(' ').slice(1).map(Number)
+        for (let i = 0; i < seeds.length; i += 2) {
+            this.seeds.push({
+                src: seeds[i],
+                len: seeds[i + 1]
+            })
+        }
+    }
+
+    mapParser(line, field) {
+        const [dst, src, len] = line.split(" ").map(Number)
+        field.push({ dst, src, len })
+    }
+
+}
+
+
+const parser = new MapParserInator()
+
+/** @param {string} line  */
+function eachLine(line) {
+    parser.parse(line)
+}
+
+function onClose() {
+    const orderOfMaps = [
+        parser.seedToSoil,
+        parser.soilToFertilizer,
+        parser.fertilizerToWater,
+        parser.waterToLight,
+        parser.lightToTemperature,
+        parser.temperatureToHumidity,
+        parser.humidityToLocation
+    ]
+    const seedRanges = parser.seeds
+    let min = Infinity
+    for (const range of seedRanges) {
+        console.log(range)
+        for (let i = range.src; i < range.src + range.len; i++) {
+            location = orderOfMaps.reduce((value, arr) => findAndMap(arr, value), i)
+            if (location < min) {
+                min = location
+            }
+        }
+    }
+    console.log(min)
+}
+/**
+ * 
+ * @param {AlmanacMap[]} arr 
+ * @param {number} value 
+ * @returns number
+ */
+function findAndMap(arr, value) {
+    const found = arr.find(m => value >= m.src && value < m.src + m.len)
+    if (!found) return value
+    return (value - found.src) + found.dst
+}
